2e465a812f226ee431f12f94d36f0b1a0b4a3cf5
Dini Hassan Youssouf <47700@eddj.local>
2024-09-11 08:49:52 +0300

    application facturation

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,186 @@
+dmx.config({
+  "manuelle": {
+    "fastscan_result": {
+      "meta": [
+        {
+          "name": "num_dossier",
+          "type": "number"
+        },
+        {
+          "name": "id_client",
+          "type": "number"
+        },
+        {
+          "name": "nom_client",
+          "type": "text"
+        },
+        {
+          "name": "adresse",
+          "type": "text"
+        },
+        {
+          "name": "nombre_compteur",
+          "type": "number"
+        },
+        {
+          "name": "support_proche",
+          "type": "text"
+        },
+        {
+          "name": "lib_long",
+          "type": "text"
+        },
+        {
+          "name": "user_crea",
+          "type": "number"
+        },
+        {
+          "name": "lib_court",
+          "type": "text"
+        },
+        {
+          "name": "date_crea",
+          "type": "datetime"
+        },
+        {
+          "name": "pl",
+          "type": "text"
+        },
+        {
+          "name": "contrat_en_cours",
+          "type": "number"
+        },
+        {
+          "name": "nom_cellule_respo",
+          "type": "text"
+        },
+        {
+          "name": "n_devis",
+          "type": "text"
+        },
+        {
+          "name": "ps",
+          "type": "text"
+        },
+        {
+          "name": "id_statut",
+          "type": "number"
+        },
+        {
+          "name": "num_cellule_respo",
+          "type": "number"
+        },
+        {
+          "name": "type_clientele",
+          "type": "number"
+        },
+        {
+          "name": "rm1",
+          "type": "number"
+        },
+        {
+          "name": "intervention_dmd",
+          "type": "number"
+        }
+      ],
+      "outputType": "text"
+    },
+    "tableRepeat1": {
+      "meta": [
+        {
+          "name": "$index",
+          "type": "number"
+        },
+        {
+          "name": "$key",
+          "type": "text"
+        },
+        {
+          "name": "$value",
+          "type": "object"
+        },
+        {
+          "name": "num_dossier",
+          "type": "number"
+        },
+        {
+          "name": "id_client",
+          "type": "number"
+        },
+        {
+          "name": "nom_client",
+          "type": "text"
+        },
+        {
+          "name": "adresse",
+          "type": "text"
+        },
+        {
+          "name": "nombre_compteur",
+          "type": "number"
+        },
+        {
+          "name": "support_proche",
+          "type": "text"
+        },
+        {
+          "name": "lib_long",
+          "type": "text"
+        },
+        {
+          "name": "user_crea",
+          "type": "number"
+        },
+        {
+          "name": "lib_court",
+          "type": "text"
+        },
+        {
+          "name": "date_crea",
+          "type": "datetime"
+        },
+        {
+          "name": "pl",
+          "type": "text"
+        },
+        {
+          "name": "contrat_en_cours",
+          "type": "number"
+        },
+        {
+          "name": "nom_cellule_respo",
+          "type": "text"
+        },
+        {
+          "name": "n_devis",
+          "type": "text"
+        },
+        {
+          "name": "ps",
+          "type": "text"
+        },
+        {
+          "name": "id_statut",
+          "type": "number"
+        },
+        {
+          "name": "num_cellule_respo",
+          "type": "number"
+        },
+        {
+          "name": "type_clientele",
+          "type": "number"
+        },
+        {
+          "name": "rm1",
+          "type": "number"
+        },
+        {
+          "name": "intervention_dmd",
+          "type": "number"
+        }
+      ],
+      "outputType": "text"
+    }
+  }
+});
